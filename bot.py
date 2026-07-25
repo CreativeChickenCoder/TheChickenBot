50dfbb24afda782e406999b0147c3c2e31e2e39c
ebkeefe5 <ebkeefe5@gmail.com>
2026-07-25 14:11:04 -0600

    commiting the bot now

---
+++ bot.py
@@ -0,0 +1,13 @@
+print("hello, welcome to the chicken bot");
+print("we are not responsible for any conversations you have with the chicken bot");
+print("We are not responsible for any actions arising out of use with the chicken bot");
+print("By using the bot, you agree to these terms");
+print(" ");
+print("hello, type quit to exit");
+while True:
+    answer = input("What's on your mind? ")
+
+    if answer == "quit":
+        break
+
+    print("bawk");
